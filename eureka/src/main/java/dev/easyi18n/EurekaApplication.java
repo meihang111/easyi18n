238be3ca6370736879d5cbc5d75a836e7b823ee1
meihang <1769881764@qq.com>
2023-05-29 23:00:13 +0800

    eureka service

---
+++ eureka/src/main/java/dev/easyi18n/EurekaApplication.java
@@ -0,0 +1,19 @@
+package dev.easyi18n;
+
+import com.netflix.discovery.EurekaNamespace;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author meihang
+ * @date 2023/5/29 22:04
+ * @project https://github.com/meihang111/easyi18n
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
